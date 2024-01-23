d44253ea5d824a828144aca9a76d206cf9938d80
DeveroCode <charli.rey.misterio@gmail.com>
2024-01-22 21:10:18 -0700

    Uodate: "adding new plugins"

--- tortas-parra-front/tailwind.config.js
+++ tortas-parra-front/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{vue}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'courgette': ['Courgette', 'cursive'],
+        'ibm-plex-sans': ['IBM Plex Sans', 'sans-serif'],
+      },
+      colors: {
+        'main': '#dfd9d9',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
